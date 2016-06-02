78e7a6bea535f02577d0b25744b3021708d3c995
Jacek Adamek <jacek.adamek@gmail.com>
2016-06-02 23:34:28 +0200

    Removing strings from decimable definition and using Kernel functions when any function meets guard conditions

--- lib/decimal_arithmetic.ex
+++ lib/decimal_arithmetic.ex
@@ -5,9 +5,9 @@ defmodule DecimalArithmetic do
   ## Examples
       iex> use DecimalArithmetic
       nil
-      iex> a = ~m/98.01/
+      iex> a = ~m(98.01)
       #Decimal<98.01>
-      iex> b = ~m/10.01/
+      iex> b = ~m(10.01)
       #Decimal<10.01>
       iex> c = a * b
       Decimal.mult(a, b)
@@ -16,22 +16,22 @@ defmodule DecimalArithmetic do
       iex> (a + b * c / d) * 3.14
       Decimal.mult(Decimal.add(a, (Decimal.div(Decimal.mult(b, c), d))), Decimal.new(3.14))
 
-      iex> net_price = ~m/34.78/
+      iex> net_price = ~m(34.78)
       #Decimal<34.78>
       iex> vat_rate = 23
       23
-      iex> net_price * (1 + vat_rate / 100) |> round(2)
+      iex> net_price * (1 + vat_rate / 100) |> Decimal.round(2)
       #Decimal<42.78>
 
   """
 
-  @type decimable :: number | String.t | Decimal.t
+  @type decimable :: number | Decimal.t
 
   @doc false
   defmacro __using__(_opts) do
     quote do
       import Kernel, except: [
-        +: 2, -: 2, *: 2, /: 2, ==: 2, !=: 2, <: 2, >: 2, <=: 2, >=: 2, rem: 2, round: 1
+        +: 2, -: 2, *: 2, /: 2, ==: 2, !=: 2, <: 2, >: 2, <=: 2, >=: 2
       ]
       import unquote(__MODULE__)
     end
@@ -41,73 +41,141 @@ defmodule DecimalArithmetic do
   Adds two decimables.
 
   ## Examples
-      iex> 1 + 3.1415
+      iex> ~m(1) + 3.1415
       #Decimal<4.1415>
+      iex> 1 + 3
+      4
   """
   @spec decimable + decimable :: Decimal.t
   def a + b do
-    Decimal.add dec(a), dec(b)
+    do_add(a, b)
   end
+
+  defp do_add(%Decimal{} = a, %Decimal{} = b) do
+    Decimal.add a, b
+  end
+  defp do_add(%Decimal{} = a, b) when is_number(b) do
+    Decimal.add a, dec(b)
+  end
+  defp do_add(a, %Decimal{} = b) when is_number(a) do
+    Decimal.add dec(a), b
+  end
+  defp do_add(a, b) do
+    Kernel.+(a, b)
+  end
+
 
   @doc """
   Subtracts two decimables.
 
   ## Examples
-      iex> 3.19 - "5.45"
+      iex> 3.19 - ~m(5.45)
       #Decimal<-2.26>
   """
   @spec decimable - decimable :: Decimal.t
   def a - b do
-    Decimal.sub dec(a), dec(b)
+    do_sub(a, b)
+  end
+
+  defp do_sub(%Decimal{} = a, %Decimal{} = b) do
+    Decimal.sub a, b
+  end
+  defp do_sub(%Decimal{} = a, b) when is_number(b) do
+    Decimal.sub a, dec(b)
+  end
+  defp do_sub(a, %Decimal{} = b) when is_number(a) do
+    Decimal.sub dec(a), b
+  end
+  defp do_sub(a, b) do
+    Kernel.-(a, b)
   end
 
   @doc """
   Multiplies decimables.
 
   ## Examples
-      iex> 7 * 2.33
+      iex> 7 * ~m(2.33)
       #Decimal<16.31>
   """
   @spec decimable * decimable :: Decimal.t
   def a * b do
-    Decimal.mult dec(a), dec(b)
+    do_mult(a, b)
+  end
+
+  defp do_mult(%Decimal{} = a, %Decimal{} = b) do
+    Decimal.mult a, b
+  end
+  defp do_mult(%Decimal{} = a, b) when is_number(b) do
+    Decimal.mult a, dec(b)
+  end
+  defp do_mult(a, %Decimal{} = b) when is_number(a) do
+    Decimal.mult dec(a), b
+  end
+  defp do_mult(a, b) do
+    Kernel.*(a, b)
   end
 
   @doc """
   Divides two decimables.
 
   ## Examples
-      iex> 3 / 4
+      iex> ~m(3) / 4
       #Decimal<0.75>
   """
   @spec decimable / decimable :: Decimal.t
   def a / b do
-    Decimal.div dec(a), dec(b)
+    do_div(a, b)
+  end
+
+  defp do_div(%Decimal{} = a, %Decimal{} = b) do
+    Decimal.div a, b
+  end
+  defp do_div(%Decimal{} = a, b) when is_number(b) do
+    Decimal.div a, dec(b)
+  end
+  defp do_div(a, %Decimal{} = b) when is_number(a) do
+    Decimal.div dec(a), b
+  end
+  defp do_div(a, b) do
+    Kernel./(a, b)
   end
 
   @doc """
   Returns true if two decimable are equal.
 
   ## Examples
-      iex> 3.15 == 3.15
+      iex> ~m(3.15) == 3.15
       true
-      iex> "5.304" == "5.304"
+      iex> ~m(5.304) == 5.304
       true
-      iex> "1.00001" == "1.00002"
+      iex> ~m(1.00001) == ~m(1.00002)
       false
   """
   @spec decimable == decimable :: boolean
   def a == b do
-    Decimal.equal? dec(a), dec(b)
+    do_eqaul(a, b)
+  end
+
+  defp do_eqaul(%Decimal{} = a, %Decimal{} = b) do
+    Decimal.equal? a, b
+  end
+  defp do_eqaul(%Decimal{} = a, b) when is_number(b) do
+    Decimal.equal? a, dec(b)
+  end
+  defp do_eqaul(a, %Decimal{} = b) when is_number(a) do
+    Decimal.equal? dec(a), b
+  end
+  defp do_eqaul(a, b) do
+    Kernel.==(a, b)
   end
 
   @doc """
   Returns true if two decimable are not equal.
 
   ## Examples
-      iex> 3.15 != 3.15
+      iex> 3.15 != ~m(3.15)
       false
-      iex> 1.00001 != 1.00002
+      iex> 1.00001 != ~m(1.00002)
       true
   """
   @spec decimable != decimable :: boolean
@@ -115,42 +183,69 @@ defmodule DecimalArithmetic do
     !__MODULE__.==(a, b)
   end
 
+  @doc """
+  Compares two decimables.
+  """
   @spec decimable > decimable :: boolean
   def a > b do
-    Kernel.==(Decimal.compare(dec(a), dec(b)), dec (1))
+    do_greater(a, b)
   end
 
+  defp do_greater(%Decimal{} = a, %Decimal{} = b) do
+    Kernel.==(Decimal.compare(a, b), dec(1))
+  end
+  defp do_greater(%Decimal{} = a, b) when is_number(b) do
+    Kernel.==(Decimal.compare(a, dec(b)), dec(1))
+  end
+  defp do_greater(a, %Decimal{} = b) when is_number(a) do
+    Kernel.==(Decimal.compare(dec(a), b), dec(1))
+  end
+  defp do_greater(a, b) do
+    Kernel.>(a, b)
+  end
+
+  @doc """
+  Compares two decimables.
+  """
   @spec decimable >= decimable :: boolean
   def a >= b do
     __MODULE__.==(a, b) || __MODULE__.>(a, b)
   end
 
+  @doc """
+  Compares two decimables.
+  """
   @spec decimable < decimable :: boolean
   def a < b do
-    Kernel.==(Decimal.compare(dec(a), dec(b)), dec(-1))
+    do_less(a, b)
   end
 
+  defp do_less(%Decimal{} = a, %Decimal{} = b) do
+    Kernel.==(Decimal.compare(a, b), dec(-1))
+  end
+  defp do_less(%Decimal{} = a, b) when is_number(b) do
+    Kernel.==(Decimal.compare(a, dec(b)), dec(-1))
+  end
+  defp do_less(a, %Decimal{} = b) when is_number(a) do
+    Kernel.==(Decimal.compare(dec(a), b), dec(-1))
+  end
+  defp do_less(a, b) do
+    Kernel.<(a, b)
+  end
+
+  @doc """
+  Compares two decimables.
+  """
   @spec decimable <= decimable :: boolean
   def a <= b do
     __MODULE__.==(a, b) || __MODULE__.<(a, b)
-  end
-
-  @spec rem(decimable, decimable) :: Decimal.t
-  def rem(a, b) do
-    {_, reminder} = Decimal.div_rem dec(a), dec(b)
-    reminder
-  end
-
-  @spec round(decimable, integer, Decimal.rounding) :: Decimal.t
-  def round(a, places \\ 0, mode \\ :half_up) do
-    Decimal.round(dec(a), places, mode)
   end
 
   @doc """
   Casts string literal to Decimal.t.
 
   ## Examples
-      iex> ~m/89.01/
+      iex> ~m[89.01]
       #Decimal<89.01>
   """
   def sigil_m(string, []) do
--- test/decimal_arithmetic_test.exs
+++ test/decimal_arithmetic_test.exs
@@ -5,48 +5,58 @@ defmodule DecimalArithmeticTest do
   doctest DecimalArithmetic
 
   test "adding" do
-    assert 1 + 3 == D.new(4)
+    assert 1.05 + ~m(3.15) == ~m(4.2)
+    assert ~m(1.05) + ~m(3.15) == ~m(4.2)
+    assert 1.05 + 3.15 == 4.2
   end
 
   test "subtracting" do
-    assert 1 - 3.0 == D.new("-2")
+    assert 1 - ~m(3.0) == ~m(-2)
+    assert ~m(1) - ~m(3.0) == ~m(-2)
+    assert 1 - 3.0 == -2
   end
 
   test "dividing" do
-    assert 1 / "3" == D.div(D.new(1), D.new(3))
+    assert 1 / ~m(3) == D.div(D.new(1), D.new(3))
   end
 
   test "multiplying" do
-    assert "1.02" * 3 == D.new("3.06")
+    assert ~m(1.02) * 3 == ~m(3.06)
   end
 
   test "compound expressions" do
-    assert D.round("1.02" * 3 / 7, 2) == D.new("0.44")
+    assert D.round(~m(1.02) * 3 / 7, 2) == ~m(0.44)
   end
 
   test "==" do
-    assert 3.001 == "3.001"
+    assert 3.001 == ~m(3.001)
   end
 
   test "!=" do
-    assert 3.002 != "3.001"
+    assert 3.002 != ~m(3.001)
   end
 
   test ">" do
+    assert ~m(3.001) > 3.0009
+    assert 3.001 > ~m(3.0009)
+    assert ~m(3.001) > ~m(3.0009)
     assert 3.001 > 3.0009
   end
 
   test ">=" do
     assert 3.001 >= 3.0009
-    assert 3.001 >= "3.001"
+    assert 3.001 >= ~m(3.001)
   end
 
   test "<" do
+    assert ~m(3.0009) < 3.001
+    assert 3.0009 < ~m(3.001)
+    assert ~m(3.0009) < ~m(3.001)
     assert 3.0009 < 3.001
   end
 
   test "<=" do
     assert 3.0009 <= 3.001
-    assert 3.001 <= 3.001
+    assert 3.001 <= ~m(3.001)
   end
 end
